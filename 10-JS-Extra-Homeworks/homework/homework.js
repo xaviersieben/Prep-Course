f92567c4eedb615c360a20c664afc06cd040cbff
xaviersieben <xaviersieben@gmail.com>
2022-04-22 17:28:51 -0300

    main

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,8 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  const entries = Object.entries(objeto);
+  return entries
 }
 
 
@@ -18,6 +20,12 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  matriz=[]
+  cuenta = 0
+  for (let i = 0; i < string.length; i++) {
+    const element = array[i];
+    
+  }
 }
 
 
